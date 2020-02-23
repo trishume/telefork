07c6ecc79c55ae370fa6b114a049290945d54ba0
Tristan Hume <tris.hume@gmail.com>
2020-02-23 13:14:10 -0500

    Remote syscalls, deleting all existing maps

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 use std::io::{Read, Write};
+use std::ffi::c_void;
 
 use libc;
 use nix;
@@ -16,6 +17,7 @@ use bincode;
 use serde::{Deserialize, Serialize};
 
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
+const PAGE_SIZE: usize = 4096;
 
 #[derive(Serialize, Deserialize)]
 struct Mapping {
@@ -33,16 +35,20 @@ enum Command {
     Registers { len: usize },
 }
 
+fn is_special_kernel_map(map: &proc_maps::MapRange) -> bool {
+    match map.filename() {
+        Some(n) if (n == "[vdso]" || n == "[vsyscall]" || n == "[vvar]") => true,
+        _ => false,
+    }
+}
+
 fn should_skip_map(map: &proc_maps::MapRange) -> bool {
     // TODO handle non-library read-only things by remapping as readable
     // TODO or maybe preserve them without contents and map zero pages on rehydrate
     if !map.is_read() || map.size() == 0 {
         return true;
     }
-    match map.filename() {
-        Some(n) if (n == "[vdso]" || n == "[vsyscall]" || n == "[vvar]") => true,
-        _ => false,
-    }
+    is_special_kernel_map(map)
 }
 
 fn error<T>(s: &'static str) -> Result<T> {
@@ -66,7 +72,7 @@ fn write_map(out: &mut dyn Write, child: Pid, map: &proc_maps::MapRange) -> Resu
 
     // === write contents
     let mut remaining_size = map.size();
-    let mut buf = vec![0u8; 4096];
+    let mut buf = vec![0u8; PAGE_SIZE];
     while remaining_size > 0 {
         let read_size = std::cmp::min(buf.len(), remaining_size);
         let offset = map.start() + (map.size() - remaining_size);
@@ -167,7 +173,7 @@ pub enum TeleforkLocation {
 
 fn _print_maps_info(maps: &[proc_maps::MapRange]) {
     for map in maps {
-        println!("{:>7} {:?}", map.size(), map.filename());
+        println!("{:>7} {:>16x} {} {:?}", map.size(), map.start(), map.flags, map.filename());
         // println!("{:?}", map);
     }
     let total_size: usize = maps.iter().map(|m| m.size()).sum();
@@ -186,11 +192,97 @@ pub fn telefork(out: &mut dyn Write) -> Result<TeleforkLocation> {
     Ok(TeleforkLocation::Parent)
 }
 
+fn single_step(child: Pid) -> Result<()> {
+    ptrace::step(child, None)?;
+    match waitpid(child, None)? {
+        WaitStatus::Stopped(_, Signal::SIGTRAP) => Ok(()),
+        _ => error("couldn't single step child"),
+    }
+}
+
 pub fn telepad(inp: &mut dyn Read) -> Result<Pid> {
     println!("incoming on telepad");
     let child: Pid = match fork_frozen_traced()? {
         NormalForkLocation::Woke => panic!("should wake up as a different process!"),
         NormalForkLocation::Parent(p) => p,
     };
+
+    let orig_maps = proc_maps::get_process_maps(child.as_raw() as proc_maps::Pid)?;
+    // _print_maps_info(&orig_maps[..]);
+
+    // FIXME find an address that is safe from both processes
+
+    // ==== poke syscall into current rip
+    let regs = ptrace::getregs(child)?;
+    let old_word: i64 = ptrace::read(child, regs.rip as *mut c_void)?;
+    // println!("old word = {:x}", old_word);
+    // SYSCALL; JMP %rax
+    let syscall_word: i64 = i64::from_ne_bytes([0x0f,0x05,0xff,0xe0,0,0,0,0]);
+    // println!("new word = {:x}", syscall_word);
+    ptrace::write(child, regs.rip as *mut c_void, syscall_word as *mut c_void)?;
+
+    // ==== remote mmap syscall to map a page there
+    let mmap_regs = libc::user_regs_struct {
+        rax: 9, // mmap
+        rdi: 0, // addr
+        rsi: PAGE_SIZE as u64, // length
+        rdx: (libc::PROT_READ | libc::PROT_WRITE | libc::PROT_EXEC) as u64, // prot
+        r10: (libc::MAP_PRIVATE | libc::MAP_ANONYMOUS) as u64, // flags
+        r8: (-1i64) as u64, // fd
+        r9: 0, // offset
+        ..regs
+    };
+    ptrace::setregs(child, mmap_regs)?;
+    single_step(child)?;
+    let regs = ptrace::getregs(child)?;
+    let mmap_location: i64 = regs.rax as i64;
+    println!("mmap location = {:x}; pre sys = {:x}; pre = {:x}", mmap_location, mmap_regs.rax as i64, regs.rax as i64);
+    if mmap_location == -1 {
+        error("failed to mmap")?;
+    }
+
+    // ==== jump to new region
+    // single_step(child)?;
+    // let regs = ptrace::getregs(child)?;
+    // if regs.rip as i64 != mmap_location {
+    //     error("jump unsuccessful")?;
+    // }
+
+    // ==== poke syscall into new page
+    ptrace::write(child, mmap_location as *mut c_void, syscall_word as *mut c_void)?;
+
+    // TODO remote munmap all original regions except vdso stuff
+    for map in &orig_maps {
+        if is_special_kernel_map(map) || map.size() == 0 {
+            continue;
+        }
+        // println!("unmapping {:?}", map);
+        let syscall_regs = libc::user_regs_struct {
+            rip: mmap_location as u64, // syscall instr
+            rax: 11, // munmap
+            rdi: map.start() as u64, // addr
+            rsi: map.size() as u64, // length
+            ..regs
+        };
+        ptrace::setregs(child, syscall_regs)?;
+        single_step(child)?;
+        let new_regs = ptrace::getregs(child)?;
+        if new_regs.rax != 0 {
+            println!("rax = {:x}; rip = {:x}", new_regs.rax, new_regs.rip);
+            error("failed to munmap")?;
+        }
+    }
+
+    println!("========== new maps:");
+    let maps = proc_maps::get_process_maps(child.as_raw() as proc_maps::Pid)?;
+    _print_maps_info(&maps[..]);
+    // TODO remote mremap vdso stuff
+
+    // TODO remote mmap new areas
+    // TODO writev new areas
+    // TODO restore registers
+    // TODO maybe jump to right place or maybe I can just restore rip
+    // TODO resume
+
     Ok(child)
 }
