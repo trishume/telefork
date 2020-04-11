ee1a81c8a9b111c6c12131e5b7107286e8134c9a
Tristan Hume <tris.hume@gmail.com>
2020-04-11 15:31:04 -0400

    Double telefork works

---
+++ examples/double.rs
@@ -0,0 +1,36 @@
+use telefork::{telefork, telepad, wait_for_exit, TeleforkLocation};
+
+use std::fs::File;
+
+fn main() {
+    println!("Hello!");
+    let mut foo = 103;
+    let fname = "dump.telefork.bin";
+    let loc = {
+        let mut output = File::create(fname).unwrap();
+        telefork(&mut output).unwrap()
+    };
+    match loc {
+        TeleforkLocation::Child => {
+            println!("hello after first telefork where foo={}", foo);
+            foo = 42;
+            let loc = {
+                let mut output = File::create("dump2.telefork.bin").unwrap();
+                telefork(&mut output).unwrap()
+            };
+            match loc {
+                TeleforkLocation::Child => {
+                    println!("hello after second telefork where foo={}", foo);
+                }
+                TeleforkLocation::Parent => println!("finished second telefork to file"),
+            };
+            std::process::exit(foo);
+        }
+        TeleforkLocation::Parent => println!("finished teleforking"),
+    };
+    let mut input = File::open(fname).unwrap();
+    let child = telepad(&mut input).unwrap();
+    println!("child pid = {}", child);
+    let status = wait_for_exit(child).unwrap();
+    println!("child exited with status = {}", status);
+}
